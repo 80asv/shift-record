9eff897bacf0ce85b7188bfd2300bc98cf8ffe70
Andres Sanabria <sanabria80.andres@gmail.com>
2023-01-07 16:43:12 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<HashRouter><App /></HashRouter>);
