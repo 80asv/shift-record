10d6ce8d7ec5c8a772f7085614f9577d40bbc4f1
Andres Sanabria <sanabria80.andres@gmail.com>
2023-01-07 21:03:57 -0500

    creating a PWA

--- src/components/HomeWrapper/HomeWrapper.jsx
+++ src/components/HomeWrapper/HomeWrapper.jsx
@@ -1,6 +1,6 @@
 import { faArrowRightFromBracket, faBook } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuth } from '../../context/AuthContext'
 import { useResize } from '../../hooks/useResize';
 import Footer from '../Footer/Footer';
@@ -10,6 +10,40 @@ const HomeWrapper = ({ children }) => {
 
     const { logout } = useAuth();
 	const { width } = useResize();
+
+	const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+	useEffect(() => {
+	window.addEventListener("beforeinstallprompt", (event) => {
+		// Prevent the mini-infobar from appearing on mobile.
+		event.preventDefault();
+		console.log("👍", "beforeinstallprompt", event);
+		// Stash the event so it can be triggered later.
+		window.deferredPrompt = event;
+		// Remove the 'hidden' class from the install button container.
+		setIsReadyForInstall(true);
+	});
+	}, []);
+
+	const handleDownloadApp = async () => {
+		console.log("👍", "butInstall-clicked");
+		const promptEvent = window.deferredPrompt;
+		if (!promptEvent) {
+			// The deferred prompt isn't available.
+			console.log("oops, no prompt event guardado en window");
+			return;
+		}
+		// Show the install prompt.
+		promptEvent.prompt();
+		// Log the result
+		const result = await promptEvent.userChoice;
+		console.log("👍", "userChoice", result);
+		// Reset the deferred prompt variable, since
+		// prompt() can only be called once.
+		window.deferredPrompt = null;
+		// Hide the install button.
+		setIsReadyForInstall(false);
+	}
 
     const handleLogout = async () => {
         try {
@@ -26,6 +60,7 @@ const HomeWrapper = ({ children }) => {
 				<FontAwesomeIcon icon={faBook} className='header__logo-icon'/>
 				<h1 className='header__logo-h1'>Registro de turnos</h1>
 			</div>
+			{isReadyForInstall && <button onClick={handleDownloadApp}>descargar</button>}
 			<button className='header__btn-cerrarsesion' onClick={handleLogout}>
 				{(width >= 660) ? 'Cerrar sesión' : <FontAwesomeIcon icon={faArrowRightFromBracket}/> }
 			</button>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<HashRouter><App /></HashRouter>);
+
+serviceWorkerRegistration.register({
+    onUpdate: async (registration) => {
+      // Corremos este código si hay una nueva versión de nuestra app
+      // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+      if (registration && registration.waiting) {
+        await registration.unregister();
+        registration.waiting.postMessage({ type: "SKIP_WAITING" });
+        // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+        window.location.reload();
+      }
+    },
+});
